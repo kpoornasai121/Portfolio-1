0a144cfd86b14d7ab94592ff58ca1e52a19a960f
kpoornasai121 <22951a05d9@iare.ac.in>
2025-03-22 17:01:41 +0530

    fifth commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+module.exports = {
+    distDir: '.next'
+  };
 
-export default nextConfig;
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
